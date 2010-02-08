baf76c8c228fb60b9eba6d13e44bba145b196b5e
Dan Rosén <danr@remote1.student.chalmers.se>
2010-02-08 10:53:34 +0100

    A first prototype for the abstract syntax tree before and after desugaring.

--- parser/STGParser.hs
+++ parser/STGParser.hs
@@ -8,34 +8,8 @@ import Text.ParserCombinators.Parsec.Prim
 import Data.Maybe
 import Control.Monad
 
--- The data type of the language, and auxilliary functions.
+import AST
 
-data Function t = Function t (Obj t)
-  deriving Show
-
-data Expr t   = EAtom (Atom t)
-              | ECall t [Atom t]
-              | ELet Bool [(t,Obj t)] (Expr t)  -- True if recursive
-              | ECase (Expr t) [Branch t]
-  deriving Show
-
-isAtom (EAtom _) = True
-isAtom _         = False
-
-data Atom t   = AVar t
-              | ANum Integer
-  deriving Show
-
-data Branch t = BCon t [t] (Expr t)
-              | BDef t (Expr t)
-  deriving Show
-
-data Obj t    = OFun [t] (Expr t)
-              | OPap (Obj t) [Atom t]   
-              | OCon t [Atom t]        
-              | OThunk (Expr t)
-              | OBlackhole
-  deriving Show
 
 -- The reserved operators
 operators :: [String]
