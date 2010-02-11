c1af4cd96125946c8a3aea2f74e7cdbb9cdf7ae0
Daniel Gustafsson <daniel.gustafsson@gmail.com>
2010-02-11 15:34:30 +0100

    Update the prettyprinter test.
    Update to the latest and greatest :)

--- parser/PrettyTest.hs
+++ parser/PrettyTest.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 module PrettyTest where
 
 import AST
@@ -11,14 +12,28 @@ import Test.QuickCheck
 newtype ParsedSTGAST = PAST (Function String)
     deriving (Show)
 
-prop_Pretty_Parse (PAST f) = case parseStg (show $ prFun f) of
-    Right [f'] -> show f == show f'
-    _ -> False
+prop_Pretty_Parse = forAllShrink arbitrary shrink $ \(PAST f) -> test_if_equal f
 
-getId = listOf1 (elements $ ['a' .. 'z'] ++ ['A' .. 'Z'])
+test_if_equal f = 
+     case parseStg (show $ prFun f) of
+        Right [f'] -> show f == show f'
+        _ -> False
+
+
+
+getId = suchThat (`elem` keywords) $
+    listOf1 (elements $ ['a' .. 'z'] ++ ['A' .. 'Z'])
+    {- case x `elem` keywords of
+        True -> getId 
+        False -> return x -}
 
 instance Arbitrary ParsedSTGAST where
     arbitrary = sized $ \s -> (\x y -> PAST $ Function x y) <$> getId <*> getObj s
+    shrink (PAST (Function x o)) = map (PAST . Function x) $ shrinkObj o
+
+instance Arbitrary (Obj String) where
+    arbitrary = sized getObj
+    shrink = shrinkObj
 
 getObj 0 = oneof
     [ OCon <$> getId <*> listOf getAtom
@@ -26,7 +41,7 @@ getObj 0 = oneof
     ]
 getObj s = oneof
     [ OFun <$> listOf1 getId <*> getExpr s'
-    --, OPap <$> getObj <*> listOf1 getAtom
+    -- , OPap <$> getObj s' <*> listOf1 getAtom
     , OCon <$> getId <*> listOf getAtom
     , OThunk <$> getExpr s'
     , pure OBlackhole
@@ -41,7 +56,7 @@ getExpr s = oneof
     [ EAtom <$> getAtom
     , ECall <$> getId <*> listOf1 getAtom
     , ELet <$> arbitrary <*> getBinds s' <*> getExpr s'
-    , ECase <$> getExpr s' <*> listOf1 (getBranch s')
+    , ECase <$> getExpr s' <*> myListOf1 s getBranch
     ]
     where s' = s `div` 2
 
@@ -50,11 +65,39 @@ getAtom = oneof
     --, ANum <$> arbitrary
     ]
 
-getBinds s = listOf1 $ (,) <$> getId <*> getObj s'
-    where s' = s `div` 2
+myListOf1 0 g = (:[]) <$> g 0
+myListOf1 s g = do
+    size <- choose (1, s)
+    replicateM size (g $ s `div` size)
+
+getBinds s = myListOf1 s $ \s' -> (,) <$> getId <*> getObj s'
 
 getBranch s = oneof
     [ BCon <$> getId <*> listOf getId <*> getExpr s'
     , BDef <$> getId <*> getExpr s'
     ]
     where s' = s `div` 2
+
+shrinkObj obj = case obj of
+    OFun args e -> OBlackhole : [OThunk e' | e' <- shrinkExpr e]
+        ++ [OFun args e' | e' <- shrinkExpr e]
+    OPap id as -> []
+    OCon _ _ -> []
+    OThunk e -> [OThunk e' | e' <- shrinkExpr e]
+    OBlackhole -> []
+
+shrinkExpr expr = case expr of
+    EAtom _ -> []
+    ECall _ _ -> []
+    ELet b binds e -> e : [ELet b binds e' | e' <- shrinkExpr e]
+        ++ [ELet b bs' e | bs' <- shrinkBinds binds]
+        -- ++ [ELet b bs' e | bs' <- shrink binds]
+    ECase scrut branches -> [scrut]
+
+shrinkBinds bs = [ [ (t, o') | o' <- shrinkObj o]
+                 | (t, o) <- bs
+                 , not (null t)]
+
+shrinkBranche br = case br of
+    BCon t args e -> [BCon t args e' | e' <- shrinkExpr e]
+    BDef t e -> [BDef t e' | e' <- shrinkExpr e]
