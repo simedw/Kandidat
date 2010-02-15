58642a4ca19471cd68b3b2318e4b0507968d693c
Daniel Gustafsson <daniel.gustafsson@gmail.com>
2010-02-15 22:16:29 +0100

    Fixed a nice test suite :)
    Now it should support the different testcases we have and be able
    to express everything Dan wants to test and report ;)

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE GADTs #-}
 module Main where
 
 import Parser.SugarParser
@@ -7,84 +6,31 @@ import Parser.Pretty.Test
 import System.Directory
 import System.FilePath
 
+import Data.List
 import Test.QuickCheck
 
-import Data.List
-
-data Test where
-  QCTest 
-    { name :: String
-    , qc   :: Property
-    } :: Test
-  PassFail
-    { name  :: String
-    , tests :: IO [a]
-    , run   :: a -> IO Bool
-    } :: Test
-  I'make'my'own'test
-    { name   :: String
-    , action :: IO Bool
-    } :: Test
-
+import Types 
 
 main :: IO ()
-main = return ()
+main = runTests
+    [ testParser
+    , QCTest { name = "parse . pretty ~= id", qc = prop_Pretty_Parse}
+    ]
 
-{-
-runTests :: [Test] -> IO ()
-runTests tests = do
-  suc <- runTests' 0 tests
-  pline
-  putStrLn $ "summary"
-  pline
-  putStrLn $ "of " ++ show (length tests) ++ ", " ++ show suc ++ " succeded"
-  where
-    title = "running: "
-    pline = putStrLn $ replicate 68 '-'
-
-    runTests' :: Int -> [Test] -> IO Int
-    runTests' n [] = return n
-    runTests' n (t : ts) = do
-      pline
-      putStrLn $ title ++ name t 
-      pline
-      res <- action t
-      case res of
-        Ok   -> runTests' (n+1) ts
-        Fail -> runTests' n ts
-
-main = do 
-    testParser 
-    runTests [ Test { name = "QuickCheck pretty", action = qcPretty}
-             , Test { name = "QuickCheck pretty 2", action = qcPretty}
-             ]
-
-testParser :: IO ()
-testParser = do
-    putStrLn "running: Parser Sugar"
-    dir <- getCurrentDirectory
-    putStrLn dir
-    let filepath = dir </> "testsuite"
-    files <- getDirectoryContents filepath
-    let files' = map (\f -> filepath ++ "/" ++ f) 
+testParser :: Test
+testParser = PassFail
+    { name = "Parser Sugar"
+    , tests = do
+        dir <- getCurrentDirectory
+        let filepath = dir </> "testsuite"
+        files <- getDirectoryContents filepath
+        return $ map (\f -> filepath ++ "/" ++ f) 
                $ filter (".hls" `isSuffixOf`) 
                $ sort files
-    res <- mapM testParse files'
-    putStrLn $ "Parser Sugar outcome : " ++ show (length $ filter id res) ++ "/" ++ show (length res)
-  where 
-    testParse file = do
+    , run = \file -> do
         str <- readFile file
         case parseSugar str of
             Right _ -> return True
             Left r  -> do putStrLn $ file ++ " fail: " ++ show r 
                           return False
-
-
-qcPretty :: TIO
-qcPretty = do 
-  res <- quickCheckResult prop_Pretty_Parse
-  case res of
-    Success l -> return Ok
-    f -> print f >> return Fail
-
--}
+    }
---
+++ test/Types.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE GADTs #-}
+module Types 
+  ( Test(..)
+  , runTests -- [Test] -> IO ()
+  ) where
+
+import Data.Ratio
+import Test.QuickCheck
+
+data Test where
+  QCTest ::  
+    { name :: String
+    , qc   :: Property
+    } -> Test
+  PassFail ::
+    { name  :: String
+    , tests :: IO [a]
+    , run   :: a -> IO Bool
+    } -> Test
+  I'make'my'own'test ::
+    { name   :: String
+    , action :: IO Bool
+    } -> Test
+
+report :: Int -> Int -> IO ()
+report suc total = putStrLn $ "outcome: " 
+    ++ show suc ++ " / " ++ show total
+
+runTest :: Test -> IO Bool
+runTest test = do
+    mapM_ putStrLn 
+        [ replicate 50 '-'
+        , "running: " ++ name test
+        , replicate 50 '-'
+        ] 
+    res <- case test of
+        QCTest name qc -> do
+            res <- quickCheckResult qc
+            case res of
+                Success l -> return True
+                f -> print f >> return False
+        PassFail name tests run -> do
+            tests' <- tests
+            res <- mapM run tests'
+            let suc   = length $ filter id res
+                total = length tests'
+            report suc total
+            return $ suc == total
+        I'make'my'own'test name action -> action
+    putStrLn $ ""
+    return res
+
+runTests :: [Test] -> IO ()
+runTests ts = do
+    res <- mapM runTest ts
+    putStrLn $ replicate 50 '%'
+    report (length (filter id res)) (length ts)
