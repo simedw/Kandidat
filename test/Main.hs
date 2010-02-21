82916a359e931334958ea5335acd6df2cc9c05cf
Simon Edwardsson <simon.edwardsson@gmail.com>
2010-02-21 22:16:24 +0100

    Added more tests - but testing is _very_ slow.

--- test/Main.hs
+++ test/Main.hs
@@ -45,50 +45,60 @@ testParser = PassFail
 -- a map between testfunctions and our semantic excepted functions
 -- lets make these test only run once! 
 testsuiteStatic = [
-    ( "ArithmTest1.hls", show $ 1 + 2)
-  , ( "ArithmTest2.hls", show $ 1 + 2 * 3 + 4 * 5)
-  , ( "ArithmTest3.hls", let twice f = f . f
-                           in show $ twice twice (+1) 2)
-  , ( "ArithmTest4.hls", show $ 3 + (2 * 3) )
-  , ( "ArithmTest5.hls", show $ 100 * (100 + 1) `div` 2)
+    ( "ArithmTest4.hls", show $ 3 + (2 * 3) )
   , ( "FunTest1.hls"   , show $ 1)
   , ( "FunTest2.hls"   , show $ 2) 
   , ( "FunTest3.hls"   , show $ 2)
   , ( "FunTest4.hls"   , show $ 2)
   , ( "FunTest6.hls"   , "(S (S Z))") -- depend on how we render results
-  , ( "ListTest1.hls"  , show $ length (replicate 3 4))
-  , ( "ListTest2.hls"  , show $ length (take 3 (repeat 4)))
   , ( "ListTest3.hls"  , let list = [5,3,1,8,2]
                            in show $ (reverse (take 2 list) 
                               == drop 3 (reverse list)))
+  , ( "PrimeTest1.hls"   , show $ True)
   , ( "OptTest1.hls"   , show $ length (take 3 (repeat 4)))
     ]
 -- note that we are working on a :: Integer -> [Integer] -> String
+-- (Filename, function, absolute value?)
 testsuiteDyn = [
-               ( "PrimeTest2.hls", \_ xs -> 
+    ( "ArithmTest1.hls", \x _ -> show $ 1 + x , False)
+  , ( "ArithmTest2.hls", \x _ -> show $ 1 + 2 * x + 4 * 5, False)
+  , ( "ArithmTest3.hls", \x _ -> let twice f = f . f
+                           in show $ twice twice (+1) x, False)
+  , ( "ArithmTest5.hls", \x _ -> show $ x * (x + 1) `div` 2, False)
+  , ( "ListTest1.hls"  , \x _ -> show $ length (replicate (fromInteger x) 4) , True)
+  , ( "ListTest2.hls"  , \x _ -> show $ length (take (fromInteger x) (repeat 4)), True)
+  , ( "PrimeTest2.hls", \_ xs -> 
                 let isprime t n = case t * t >= n of
                         True  -> True
                         False -> if (n `mod` t == 0) 
                                   then False
                                   else isprime (t+1) n
                  in show $ all (isprime 2) xs
-              )
+              , False)
             ]
 
 -- create our test cases
+-- This is slow, so we have to think of ways to make it faster.
 interpreter = map toTestStatic testsuiteStatic ++ map toTestDyn testsuiteDyn
   where 
-    toTestDyn (file, fun) = 
+    toTestDyn (file, fun, a) = 
         QCTest { name   = file
-             , qc   = forAll arbitrary $ \(x :: Integer) (y :: [Integer]) -> 
-              let input = defaultInput { inputInteger  = Just x
+             , qc   = forAll arbitrary $ \(x :: Integer) (y :: [Integer]) ->
+              let x' = cap 1000 $ abs' a x
+                  input = defaultInput { inputInteger  = Just x'
                                        , inputIntegers = Just y
                                        }
                   setting = defaultSettings {input = input}
                   -- can we lift IO in some better way?
                   v = unsafePerformIO $ forceInterpreter setting file 
-                                 in not (null y) ==> v == (fun x y)
+                                 in v == (fun x' y)
                }
+    abs' True  x | x < 0     = -x
+    abs' True  x | otherwise = x
+    abs' False x = x 
+    cap y x | x > y     = y
+    cap y x | otherwise = x
+
     toTestStatic (file, value) = 
         I'make'my'own'test { name = file
              , action   = do 
