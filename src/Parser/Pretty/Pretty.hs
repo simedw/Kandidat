a7ad8acc3a5e49591ad601c32d221c772ae9a0cf
Daniel Gustafsson <daniel.gustafsson@gmail.com>
2010-02-27 00:03:43 +0100

    Big commit, new interpreter, and pretty printer.
    A new interpreter that allows breakpoints, it also allows the user
    to step both forward and backwards int the history.
    
    The pretty printer now follows our coding convention for STG, it
    also does this in colour.
    
    Please remember to cabal configure -ftest, since we have updated
    the dependencies.

--- src/Parser/Pretty/Pretty.hs
+++ src/Parser/Pretty/Pretty.hs
@@ -2,7 +2,7 @@ module Parser.Pretty.Pretty where
 
 import Stg.AST
 
-import Text.PrettyPrint
+import Text.PrettyPrint.ANSI.Leijen
 
 prFun :: Function String -> Doc
 prFun = prFunction text
@@ -10,46 +10,65 @@ prFun = prFunction text
 prFuns = seppis . map prFun
 
 -- prFunction :: (t -> Doc) -> Function t -> Doc
-prFunction prVar (Function id obj) = prVar id <+> equals <+> prObj prVar obj
+prFunction prVar (Function id obj) = var prVar id <+> equal <+> prObj prVar obj
 
 -- prExpr :: (t -> Doc) -> Expr t -> Doc
 prExpr prVar e = case e of
     EAtom atom -> prAtom prVar atom
-    ECall id as -> prVar id <+> hsep [ prAtom prVar a | a <- as ]
-    ELet b binds e -> (text $ case b of
+    ECall id as -> var prVar id <+> hsep [ prAtom prVar a | a <- as ]
+    ELet b binds e -> (key $ case b of
         True  -> "letrec"
-        False -> "let" )<+> braces (nest 0 (prLetBind prVar binds)) $$ text "in" <+> prExpr prVar e
-    ECase scrut binds -> text "case" <+> prExpr prVar scrut <+> text "of"
-        <+> braces (text "" $+$ nest 2 (seppis $ map (prBind prVar) binds))
-    EPop op as -> text (show op) <+> text "#" <+> hsep [ prAtom prVar a | a <- as ]  
+        False -> "let" ) <$>  indent 4 (prLetBind prVar binds) 
+                           <+> key "in" <+> prExpr prVar e
+    ECase scrut binds -> key "case" <+> prExpr prVar scrut <+> key "of"
+        <$> indent 4 (mkBrace $ map (prBind prVar) binds)
+    EPop op as -> operator (show op) <> operator "#" <+> hsep [ prAtom prVar a | a <- as ]  
 
-seppis = vcat . punctuate semi
+equal   = magenta equals
+operator = magenta . text
+key = yellow . text
+conVar prVar  = bold . green . prVar
+bindVar prVar = cyan . prVar
+symbol        = red
+otext = bold . blue . text
+var   = (green .)
+num   = green
+mparens = enclose (symbol lparen) (symbol rparen)
+mbraces = enclose (symbol lbrace) (symbol rbrace)
+
+seppis = vcat . punctuate (text "" <$> symbol semi <+> text "")
+
+mkBrace [] = symbol $ braces empty
+mkBrace (x : xs) = symbol lbrace <+> x <$> mkBrace' xs
+  where
+    mkBrace' []     = symbol rbrace
+    mkBrace' (x:xs) = symbol semi <+> x <$> mkBrace' xs
 
 prBind prVar bind = case bind of
-    BCon name args e -> prVar name <+> hsep (map prVar args) <+> text "->" 
+    BCon name args e -> conVar prVar name <+> hsep (map (bindVar prVar) args) <+> operator "->" 
          <+> prExpr prVar e
-    BDef name e -> braces (prVar name) <+> text "->" <+> prExpr prVar e
+    BDef name e -> mbraces (bindVar prVar name) <+> operator "->" <+> prExpr prVar e
 
 -- prBind :: (t -> Doc) -> [(t, Obj t)] -> Doc
-prLetBind prVar binds = seppis
-    [ prVar x <+> equals <+> prObj prVar obj
+prLetBind prVar binds = mkBrace
+    [ bindVar prVar x <+> equal <+> prObj prVar obj
     | (x, obj) <- binds ]
+
 
 -- prAtom :: (t -> Doc) -> Atom t -> Doc
 prAtom prVar atom = case atom of
-    AVar x -> prVar x
-    ANum n -> integer n
-    ADec f -> double f
+    AVar x -> var prVar x
+    ANum n -> num $ integer n
+    ADec f -> num $ double f
 
 -- prObj :: (t -> Doc) -> Obj t -> Doc
 prObj prVar obj = case obj of
-    OFun args e -> text "FUN" <+> parens (hsep (map prVar args) <+> text "->" 
+    OFun args e -> otext "FUN" <+> mparens (hsep (map (bindVar prVar) args) <+> operator "->" 
                                           <+> prExpr prVar e)
-    OPap obj args -> text "PAP" <+> parens (prVar obj 
+    OPap obj args -> otext "PAP" <+> mparens (var prVar obj 
                                             <+> hsep (map (prAtom prVar) args))
-    OCon name args -> text "CON" <+> parens (prVar name 
+    OCon name args -> otext "CON" <+> mparens (conVar prVar name 
                                              <+> hsep (map (prAtom prVar) args))
-    OThunk e -> text "THUNK" <+> parens (prExpr prVar e)
-    OOpt   a -> text "OPT" <+> parens (prAtom prVar a)
-    OBlackhole -> text "BLACKHOLE"
-
+    OThunk e -> otext "THUNK" <+> mparens (prExpr prVar e)
+    OOpt   a -> otext "OPT" <+> mparens (prAtom prVar a)
+    OBlackhole -> otext "BLACKHOLE"
--- test/Interpreter.hs
+++ test/Interpreter.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE PatternGuards #-}
 module Interpreter where
 
+import "mtl" Control.Monad.State
 import Data.Function
 import Data.List
 import qualified Data.Map as M
@@ -8,12 +11,16 @@ import System( getArgs )
 import System.Console.GetOpt
 import System.Directory
 import System.FilePath
-import Text.PrettyPrint
+import qualified Text.PrettyPrint.ANSI.Leijen as PP
+
+import System.Console.Haskeline (InputT, outputStrLn, getInputLine) 
+import qualified System.Console.Haskeline as Hl
 
 import Parser.Diabetes
 import Parser.Pretty.Pretty
 import Parser.SugarParser
 import Stg.AST
+import Stg.GC
 import Stg.Input
 import Stg.Interpreter
 import Stg.PrePrelude
@@ -21,19 +28,34 @@ import Stg.Rules
 import Stg.Types
 
 data Settings = Settings
-  { steping :: Bool
-  , prelude :: String
+  { steping      :: Bool
+  , prelude      :: String
   , showStgState :: (StgState String -> String)
   , showStgRule  :: (Rule -> String)
-  , input :: Input
-  , quiet :: Bool
+  , input        :: Input
+  , quiet        :: Bool
+  , forceStg     :: Bool
+  , toGC         :: Bool
   }
+
+type Result = (Rule, StgState String)
+
+data BreakPoint
+    = AtRule Rule
+    | AtCall String
+    deriving (Eq, Show)
+
+evalBP :: BreakPoint -> Result -> Bool
+evalBP bp (rule, StgState code stack heap) = case bp of
+    AtRule r -> r == rule
+    AtCall id -> case code of 
+        ECall fid _ -> id == fid
 
 stgState :: Bool -> Bool -> Bool -> StgState String -> String
 stgState sc ss sh st@(StgState {code, stack, heap}) = 
-       showi sc "code" (show (prExpr (text . show) code))
+       showi sc "code" (show (prExpr PP.text code))
     ++ showi ss ("stack(" ++ show (length stack) ++ ")") (show stack) 
-    ++ showi sh ("heap("++ show (M.size heap) ++")") (concat [ show (id, prObj (text . show) obj) ++ "\n\t"
+    ++ showi sh ("heap("++ show (M.size heap) ++")") (concat [ show (id, prObj PP.text obj) ++ "\n\t"
                              | (id, obj) <- M.toList heap])
   where
     showi :: Bool -> String -> String -> String
@@ -43,16 +65,25 @@ stgState sc ss sh st@(StgState {code, stack, heap}) =
 
 
 defaultSettings = Settings {
-    steping = True
-  , prelude = "Prelude.hls"
+    steping      = True
+  , prelude      = "Prelude.hls"
   , showStgState = stgState True True True
-  , showStgRule = show 
-  , input = defaultInput
-  , quiet = False
+  , showStgRule  = show 
+  , input        = defaultInput
+  , quiet        = False
+  , forceStg     = False
+  , toGC         = True
   }
 
 noheapSettings = defaultSettings { showStgState = stgState True True False }
 
+
+data InterpreterState = IS
+    { settings :: Settings
+    , stgm     :: StgMState String
+    , history  :: [Result]
+    , breakPoints  :: [BreakPoint]
+    }
 
 -- forceInterpreter forces the return value to be evaluted
 -- so we can display it in a nice fashion
@@ -68,35 +99,190 @@ forceInterpreter settings file = do
       Left  r  -> do return $ "fail: " ++ show r
 
 
-
 testInterpreter :: Settings -> FilePath -> IO ()
-testInterpreter settings file = do
+testInterpreter set file = do
     dir     <- getCurrentDirectory
-    prelude <- readFile (dir </> ".." </> "prelude" </> prelude settings)
-    res     <- readFile (dir </> ".." </> "testsuite" </> file)
+    prelude <- readFile (dir </> "prelude" </> prelude set)
+    res     <- readFile (dir </> "testsuite" </> file)
     -- prelude must be last, otherwise parse error messages get wrong line numbers!
     case parseSugar (res ++ prelude) of  
       Right fs -> do
-        let trace = eval (input settings) (prePrelude ++ run fs)
-        mapM_ (\(r, s) -> putStrLn "<-------------->" 
-                        >> steps (steping settings) 
-                        >> putStrLn (showStgRule settings r 
-                        ++ "\n" 
-                        ++ showStgState settings s)) trace
-        print $ length trace
-        print $ map (\ list -> (head list, length list))
-              $ group $ sort $ map fst  trace
+        let st = initialState ( createGetFuns (input set)
+                              ++ prePrelude
+                              ++ run fs)
+        evalStateT (Hl.runInputT Hl.defaultSettings (loop st)) IS
+            { settings = set
+            , stgm     = initialStgMState
+            , history  = []
+            , breakPoints = []
+            } 
       Left  r  -> do putStrLn $ "fail: " ++ show r
+
+loop :: StgState String -> InputT (StateT InterpreterState IO) ()
+loop originalState  = do
+    minput <- getInputLine "% " 
+    set <- lift $ gets settings
+    let st@(StgState code stack heap) = (if toGC set then gc else id) originalState
+    case minput of
+        Nothing -> return ()
+        Just xs -> case words xs of
+            [] -> evalStep 1 st
+            [":q"]   -> return ()
+            ":v" : xs    -> loopView st xs
+            ":view" : xs -> loopView st xs
+            ":bp"  : xs -> addbp xs >> loop st
+            [":back", num] -> case reads num of
+                ((x, "") : _) -> evalStep (negate x) st
+                _ -> loop st
+            [":step", num] -> case reads num of
+                ((x, "") : _) -> evalStep x st
+                _ -> loop st
+            [":h"] -> printHelp >> loop st
+            [":help"] -> printHelp >> loop st
+            input -> do
+                outputStrLn $ "oh hoi: " ++ unwords input
+                loop st
   where
-    steps True  = getChar >> return ()
-    steps False = return ()
+    gc = mkGC ["$True", "$False"]
+
+    bp :: [BreakPoint] -> Result -> Maybe BreakPoint
+    bp [] _ = Nothing
+    bp (b : bs) r | evalBP b r = Just b
+                  | otherwise  = bp bs r
+
+    evalStep n s | n == 0 = printSummary s
+                 | n < 0  = do
+        hist <- lift . gets $ history
+        case hist of
+            [] -> printSummary s
+            (_, s') : xs -> do
+                lift . modify $ \set -> set { history = xs }
+                evalStep (n + 1) s'
+                 | otherwise = do
+        stg <- lift $ gets stgm
+        bps <- lift . gets $ breakPoints
+        case runState (step s) stg of
+            (Nothing, stg') -> do
+                outputStrLn "No Rule applied"
+                loop s
+            (Just res@(_, s'), stg') | Just b <- bp bps res -> do
+                addHistory res
+                lift . modify $ \set -> set { stgm = stg' }
+                outputStrLn $ "BreakPoint! " ++ show b
+                printSummary s'
+                                     | otherwise -> do
+                addHistory res
+                lift . modify $ \set -> set { stgm = stg' }
+                evalStep (n - 1) s'
+                
+    addHistory res = lift . modify $ \set -> set { history = res : history set }
+
+    addbp xs = case xs of
+        ["rule", r] -> case reads r of 
+            (r', "") : _ -> do
+                outputStrLn "ok"
+                lift . modify $ \set -> set { breakPoints = AtRule r' : breakPoints set }
+            _ -> outputStrLn "can't parse that rule"
+        ["call", f] -> do
+            outputStrLn "ok"
+            lift . modify $ \set -> set { breakPoints = AtCall f : breakPoints set }
+        _ -> outputStrLn "Sirisly u want m3 too parse that?"
+
+    printSummary st@(StgState code stack heap) = do
+        hist <- lift (gets history)
+        case hist of
+            [] -> do
+                outputStrLn $ "Rule: " ++ show RInitial
+                printCode code
+                printStack stack
+                outputStrLn $ "heap("  ++ show (M.size heap) ++ ")"
+                loop st
+            (rule, StgState code stack heap) : _ -> do
+                outputStrLn $ "Rule: " ++ show rule
+                printCode code
+                printStack stack
+                outputStrLn $ "heap("  ++ show (M.size heap) ++ ")"
+                loop st
+                
+
+    loopView st@(StgState code stack heap) xs = do
+        case xs of
+            ["h"]    -> printHeap heap
+            ["heap"] -> printHeap heap
+            ["set"]  -> printSetting =<< lift (gets settings)
+            ["settings"] -> printSetting =<< lift (gets settings)
+            ["s"]     -> printStack stack
+            ["stack"] -> printStack stack
+            ["c"]     -> printCode code
+            ["code"]  -> printCode code
+            ["rules"] -> printRules
+            ["sum"]   -> printSummary st
+            ["bp"]    -> do
+                bps <- lift (gets breakPoints)
+                outputStrLn $ show bps
+            _ -> outputStrLn "wouldn't we all?"
+        loop st
+
+    printSetting set = do
+        outputStrLn "Current Settings:"
+        mapM_ (outputStrLn . show) 
+            [ "steping" ~> steping set
+            , "quiet"    ~> quiet set
+            , "forceStg" ~> forceStg set
+            , "toGC"     ~> toGC set
+            ]
+      where
+        (~>) = (,)
+
+    printCode code = do
+        outputStrLn $ "code: " ++ show (prExpr PP.text code)
+
+    printStack stack = do
+        outputStrLn $ "stack(" ++ show (length stack) ++ "): " ++ show stack 
+
+    printHeap heap = do
+        outputStrLn $ "heap(" ++ show (M.size heap) ++ "): \n" 
+                    ++ concat [ foo (id, show $ prObj PP.text obj)
+                              | (id, obj) <- M.toList heap]
+      where
+        foo :: (String, String) -> String
+        foo (id, obj) = unlines . map ("  " ++) $ case lines obj of
+            []     -> []
+            x : xs -> (id ++ " = " ++ x) 
+                        : map (\y -> replicate (length id + 3) ' ' ++ y) xs
+            
+    printRules = do
+        ruls <- lift $ gets history
+        outputStrLn $ "number of rules: " ++ show (length ruls)
+        outputStrLn . show . map (\ list -> (head list, length list))
+                    . group . sort . map fst $ ruls 
+
+    printHelp = do
+        mapM_ outputStrLn
+            [ "Oh hi! This is the Interpreter speaking"
+            , "Press the following to do the following"
+            , ":q - to quit"
+            , ":v or :view - to view something"
+            , "   h or heap  - the heap"
+            , "   s or stack - the stack"
+            , "   c or code  - the code"
+            , "   rules      - the number of rules fired"
+            , "   sum        - a summary"
+            , "   bp         - the current breakpoints"
+            , ":bp - to add a new breakpoint"
+            , "   rule <rule> - for that <rule>"
+            , "   call <id>   - when calling <id>"
+            , "step <n> - step <n> steps :)"
+            , "back <n> - step <-n> steps (history)"
+            , ""
+            , "Happy Hacking !!"
+            ]
 
 main :: IO ()
 main = do
     -- First argument is the filename
     file:args <- getArgs
     print $ args
---    let ( flags, nonOpts, msgs ) = getOpt RequireOrder options args
     case getOpt RequireOrder options args of
         (flags, [],      [])     -> do
              opts <- foldl (>>=) (return defaultSettings) flags
@@ -109,7 +295,8 @@ data Flag = Version
 options :: [OptDescr (Settings -> IO Settings)] 
 options = 
     [ Option ['S'] ["step"] (ReqArg setSteping "BOOL") "step through"
-    , Option ['V'] ["visible"] (ReqArg setVisible "BOOL BOOL BOOL") "show code stack heap"
+    , Option ['F'] ["force"] (ReqArg setForce "Bool") "force evaluation"
+    , Option ['V'] ["visible"] (ReqArg setVisible "\"BOOL BOOL BOOL\"") "show code stack heap"
     , Option ['I'] ["integerinput"] 
         (ReqArg setInputInteger "Integer") 
         "single integer input"
@@ -129,6 +316,8 @@ setVisible ind set = let (arg: arg2: arg3:_) = words ind
                          c = read arg
                      in return $ set {  showStgState =  stgState c s h }
 
+setForce :: String -> Settings -> IO Settings
+setForce arg set = return $ set { forceStg = read arg }
 
 
 setInputInteger :: String -> Settings -> IO Settings
